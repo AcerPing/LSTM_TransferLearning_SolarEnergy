795ebe569c6e9799f71c4658759b52ee24f9b249
LabComputer\HoChePing <t111c71008@ntut.org.tw>
2025-06-27 16:01:29 +0800

    2025-06-27 資料處理 Plant1第一號發電機組

--- preprocess/Plant1第一號發電機組/read.py
+++ preprocess/Plant1第一號發電機組/read.py
@@ -1,7 +1,8 @@
 import pickle
-from os import listdir, path
+import os
 
-with open('X_train','rb') as f:
+folder = r'20%Training & 80%Testing' # 80%Training & 20%Testing # 20%Training & 80%Testing
+with open(os.path.join('.', folder, 'X_test.pkl'),'rb') as f:
     data = pickle.load(f)
 
 print(len(data))
